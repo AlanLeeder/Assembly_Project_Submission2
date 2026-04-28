f4b42f23a33ca24676a84e51d8d842e0c1d3311b
AlanLeeder <C00311483@setu.ie>
2026-04-28 23:52:34 +0100

    Update test.c

--- PROJECT2/test.c
+++ PROJECT2/test.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <assert.h>
 
-// declare the assembly function
 long REGISTER_ADDER(long a, long b);
 
 int main() {
 
     long result;
 
-    // Test 1
     result = REGISTER_ADDER(2, 3);
     printf("2 + 3 = %ld\n", result);
     assert(result == 5);
 
-    // Test 2
     result = REGISTER_ADDER(-10, 5);
     printf("-10 + 5 = %ld\n", result);
     assert(result == -5);
 
-    // Test 3
     result = REGISTER_ADDER(100, 200);
     printf("100 + 200 = %ld\n", result);
     assert(result == 300);
